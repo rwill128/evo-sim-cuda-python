48d201adb2979dbeb7eb5cf980a1e19ffee1c56d
rwilliams <rwilliams@casenetllc.com>
2020-10-23 12:41:39 -0400

    Improving land and water code.

--- asset-generation/land_creation.py
+++ asset-generation/land_creation.py
@@ -2,8 +2,8 @@ import numpy as np
 import scipy.signal as signal
 
 
-def create_land() -> np.ndarray:
-    return np.random.random(size=(800, 800))
+def create_template(x=800, y=800) -> np.ndarray:
+    return np.random.random(size=(x, y))
 
 
 def add_smoothing_to_land(l: np.ndarray):
@@ -12,11 +12,19 @@ def add_smoothing_to_land(l: np.ndarray):
     return sl
 
 
-def lower_all_land_below_threshold(l2: np.ndarray, t: float) -> np.ndarray:
-    lowered_land = l2.copy()
+def generate_land_and_water_from_template(l2: np.ndarray, t: float) -> (np.ndarray, np.ndarray):
+    lowered_template = l2.copy()
+    threshold_array = np.full(np.shape(lowered_template), t)
 
-    for item in np.nditer(lowered_land, op_flags=['readwrite']):
-        if item < t:
-            item[...] = 0
+    lowered_template = np.subtract(lowered_template, threshold_array)
+    land = np.clip(lowered_template, 0.0, 1.0)
+    water = np.clip(lowered_template, -1.0, 0.0)
+    return land, water
 
-    return lowered_land
+
+def entire_surface(l: np.ndarray, w: np.ndarray) -> np.ndarray:
+    zeros = np.zeros(w.shape)
+    non_zero_locations_in_water = np.not_equal(w, zeros)
+    ef = l.copy()
+    ef = np.copyto(ef, w, where=non_zero_locations_in_water)
+    return ef
--- asset-generation/land_creation_test.py
+++ asset-generation/land_creation_test.py
@@ -1,13 +1,25 @@
-import sys
 import unittest
 import land_creation
 
 
 class LandCreationTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.template = land_creation.create_template(800, 800)
+        self.smoothed_template = land_creation.add_smoothing_to_land(self.template)
+        self.land, self.water = land_creation.generate_land_and_water_from_template(self.smoothed_template, 0.5)
+
     def test_create_land(self):
-        print(sys.path)
-        land = land_creation.create_land()
-        self.assertEqual(land.size, 640000)
+        self.assertEqual(self.template.size, 640000)
+
+    def test_lower_all_land_below_threshold(self):
+        self.assertGreaterEqual(self.land.min(), 0.0)
+        self.assertLessEqual(self.land.max(), 0.5)
+        self.assertGreaterEqual(self.water.min(), -0.5)
+        self.assertLessEqual(self.water.max(), 0.0)
+
+    def test_entire_surface(self):
+        entire_surface = land_creation.entire_surface(self.land, self.water)
 
 
 if __name__ == '__main__':
