b00731a80ba1702c65bf2cf0148695bf012f0011
Rick <rwill128.create@gmail.com>
2020-10-24 11:30:56 -0400

    Making slight tweaks to rendering code.

--- visualization/array_rendering.py
+++ visualization/array_rendering.py
@@ -6,5 +6,5 @@ import numpy as np
 def render_array(l: np.ndarray):
     fig4 = plt.figure()
     ax4 = fig4.add_subplot(122)
-    ax4.imshow(l, interpolation='nearest', cmap=cm.Greys_r)
+    ax4.imshow(l, interpolation='nearest', cmap=cm.Greens)
     plt.show()
