a8581ef589c34cc5dca77e5fbd67b9a3e267fd11
Rick <rwill128.create@gmail.com>
2020-10-24 12:27:34 -0400

    Adding random spawning code.

--- visualization/creature_rendering.py
+++ visualization/creature_rendering.py
@@ -46,3 +46,28 @@ def place_creature(t, c):
     for l in c[1:]:
         segment_id = l[0]
         detect_occluded_squares(t, l[1:], c_id + segment_id)
+
+
+def rotate_vector(x, y, t):
+    rot_mat = np.zeros(shape=(2, 2))
+    vector = np.zeros(shape=(1, 2))
+    rot_mat = [[np.cos(t), -np.sin(t)],
+               [np.sin(t), np.cos(t)]]
+    vector = [[x], [y]]
+    rotated_vector = np.matmul(rot_mat, vector)
+    return rotated_vector[0, 0], rotated_vector[1, 0]
+
+
+def translate_creature_segs_to_world(c: np.ndarray):
+    translated_c = c.copy()
+    x_translation = c[0, 1]
+    y_translation = c[0, 2]
+    theta = c[0, 3]
+
+    for line in translated_c[1:]:
+        rot_x, rot_y = rotate_vector(line[1], line[2], theta)
+        rot_x2, rot_y2 = rotate_vector(line[3], line[4], theta)
+        line[1], line[2] = rot_x + x_translation, rot_y + y_translation
+        line[3], line[4] = rot_x2 + y_translation, rot_y2 + y_translation
+
+    return translated_c
