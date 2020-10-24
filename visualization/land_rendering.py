8e57c20cff930dc06d6128364e6da90196d8b372
Richard Williams <rwill128.create@gmail.com>
2020-10-23 22:37:54 -0400

    Finally got creature module sorted out

--- visualization/land_rendering.py
+++ visualization/land_rendering.py
@@ -3,7 +3,7 @@ import matplotlib.cm as cm
 import numpy as np
 
 
-def render_land(l: np.ndarray):
+def render_array(l: np.ndarray):
     fig4 = plt.figure()
     ax4 = fig4.add_subplot(122)
     ax4.imshow(l, interpolation='nearest', cmap=cm.Greys_r)
