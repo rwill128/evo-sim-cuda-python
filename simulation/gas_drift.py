8f5e5d9126dfe49841f7a0c50f06823a4c507786
Richard Williams <rwill128.create@gmail.com>
2020-10-27 15:42:26 -0400

    Formatting cleanup.

--- simulation/gas_drift.py
+++ simulation/gas_drift.py
@@ -17,6 +17,6 @@ def move_gases(world_params):
             possible_places_to_go.append((x, y + 1))
 
         if len(possible_places_to_go) > 0:
-            i, j = possible_places_to_go[np.random.randint(0, len(possible_places_to_go))]
+            i, j = possible_places_to_go[np.random.randint(0, len(possible_places_to_go)) - 1]
             world_params['carbon_dioxide_map'][x][y] -= 1
             world_params['carbon_dioxide_map'][i][j] += 1
--- visualization/array_rendering.py
+++ visualization/array_rendering.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import matplotlib.cm as cm
 import numpy as np
 
 
