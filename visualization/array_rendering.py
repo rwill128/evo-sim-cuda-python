0ce444564f1ef06fc3537735424a605072a588c4
rwilliams <rwilliams@casenetllc.com>
2020-10-27 11:40:06 -0400

    Trying to improve rendering code. Not working so far.

--- visualization/array_rendering.py
+++ visualization/array_rendering.py
@@ -8,3 +8,18 @@ def render_array(l: np.ndarray):
     ax4 = fig4.add_subplot(122)
     ax4.imshow(l, interpolation='nearest', cmap=cm.Greens)
     plt.show()
+
+
+def display_world(world_params):
+    fig: object = plt.figure()
+    fig.suptitle('The World')
+
+    plant_im = fig.add_subplot(122)
+    plant_im.title.set_text("Plants")
+    plant_im.imshow(world_params['world_array'], interpolation='nearest', cmap=cm.Greens)
+
+    ax4 = fig.add_subplot(122)
+    ax4.title.set_text("Carbon Dioxide")
+    ax4.imshow(world_params['carbon_dioxide_map'], interpolation='nearest', cmap=cm.Greens)
+
+    plt.show()
