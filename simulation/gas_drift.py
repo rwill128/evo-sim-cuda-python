bf7d778b3d6442b6e49a18e4fde969254cfdf320
Richard Williams <rwill128.create@gmail.com>
2020-10-29 01:02:03 -0400

    Playing with scipy sparse matrix representations. So far they are much slower.

--- simulation/gas_drift.py
+++ simulation/gas_drift.py
@@ -1,7 +1,10 @@
+import math
+
 import numpy as np
 import numba as nb
 from numba import prange
 import scipy.sparse as sparse
+import sys
 
 
 @nb.jit(nopython=True, fastmath=True)
@@ -29,56 +32,52 @@ def move_gases(gas_map: np.array, world_size: int):
 
 # @nb.jit(nopython=True, fastmath=True)
 def move_gases_scipy_sparse_matrix(gas_map: sparse.csc_matrix, world_size: int):
+    sys.setrecursionlimit(10000)
     gas_filled_squares = gas_map.nonzero()
-    row = np.array([])
-    col = np.array([])
-    data = np.array([])
+    gas_values = sparse.csc_matrix(gas_map[gas_filled_squares]).toarray()
 
-    for parallel_index in range(len(gas_filled_squares[0])):
-        x = gas_filled_squares[0][parallel_index]
-        y = gas_filled_squares[1][parallel_index]
-        possible_places_to_go = []
-        gas_value = gas_map.getrow(x).getcol(y).data[0]
+    row_test: np.array = np.array([])
+    col_test: np.array = np.array([])
+    data_test: np.array = np.array([])
+    row_test, col_test, data_test = numba_move_gases(gas_filled_squares[0], gas_filled_squares[1], gas_values,
+                                                     row_test.copy(), col_test.copy(),
+                                                     data_test.copy(), world_size)
 
-        if x > 0 and len(gas_map.getrow(x - 1).getcol(y).data) > 0 and gas_map.getrow(x - 1).getcol(y).data[0] < gas_value:
-            possible_places_to_go.append((x - 1, y))
-        else:
-            if len(gas_map.getrow(x - 1).getcol(y).data) == 0:
-                possible_places_to_go.append((x - 1, y))
-
-        if x < world_size - 1 and len(gas_map.getrow(x + 1).getcol(y).data) > 0 and gas_map.getrow(x + 1).getcol(y).data[0] < gas_value:
-            possible_places_to_go.append((x + 1, y))
-        else:
-            if len(gas_map.getrow(x + 1).getcol(y).data) == 0:
-                possible_places_to_go.append((x + 1, y))
+    modifications_matrix = sparse.coo_matrix((data_test, (row_test, col_test)), shape=(world_size, world_size)).tocsc()
+    return gas_map + modifications_matrix
 
 
-        if y > 0 and len(gas_map.getrow(x).getcol(y - 1).data) > 0 and gas_map.getrow(x).getcol(y - 1).data[0] < gas_value:
-            possible_places_to_go.append((x, y - 1))
-        else:
-            if len(gas_map.getrow(x).getcol(y - 1).data) == 0:
-                possible_places_to_go.append((x, y - 1))
+# @nb.jit(nopython=True, fastmath=True)
+def numba_move_gases(x_squares: np.array, y_squares: np.array, gas_values: np.array, row_test: np.array,
+                     col_test: np.array, data_test: np.array, world_size: int):
+    for parallel_index in range(len(x_squares)):
+        x = x_squares[parallel_index]
+        y = y_squares[parallel_index]
+        value = gas_values[0, parallel_index]
 
+        row_test = np.append(row_test, x)
+        col_test = np.append(col_test, y)
+        data_test = np.append(data_test, -1)
 
-        if y < world_size - 1 and len(gas_map.getrow(x).getcol(y + 1).data) > 0 and gas_map.getrow(x).getcol(y + 1).data[0] < gas_value:
-            possible_places_to_go.append((x, y + 1))
-        else:
-            if len(gas_map.getrow(x).getcol(y + 1).data) == 0:
-                possible_places_to_go.append((x, y + 1))
+        up = np.array([x, y + 1])
+        down = np.array([x, y - 1])
+        left = np.array([x - 1, y])
+        right = np.array([x + 1, y])
+        choice_mask = np.array([up, down, right, left])
+        choices = np.random.choice(np.array([0, 1, 2, 3]), 4)
 
-        if len(possible_places_to_go) > 0:
-            i, j = possible_places_to_go[np.random.randint(0, len(possible_places_to_go))]
-            row = np.append(row, x)
-            col = np.append(col, y)
-            data = np.append(data, -1)
-
-            row = np.append(row, i)
-            col = np.append(col, j)
-            data = np.append(data, 1)
-
-            # gas_map[x][y] -= 1
-            # gas_map[i][j] += 1
-
-    modifications_matrix = sparse.coo_matrix((data, (row, col)), shape=(world_size, world_size))
-
-    gas_map = gas_map + modifications_matrix
+        for choice in choices:
+            i, j = choice_mask[choice]
+            if len(np.where(x_squares == i)[0]) == 0 or len(
+                    np.where(y_squares[np.where(x_squares == i)[0]] == j)[0]) == 0:
+                row_test = np.append(row_test, min(world_size - 1, max(0, i)))
+                col_test = np.append(col_test, min(world_size - 1, max(0, j)))
+                data_test = np.append(data_test, 1)
+                break
+            else:
+                if gas_values[0, np.where(y_squares[np.where(x_squares == i)[0]] == j)[0][0]] < value:
+                    row_test = np.append(row_test, min(world_size - 1, max(0, i)))
+                    col_test = np.append(col_test, min(world_size - 1, max(0, j)))
+                    data_test = np.append(data_test, 1)
+                    break
+    return row_test, col_test, data_test
--- simulation/gas_drift_test.py
+++ simulation/gas_drift_test.py
@@ -10,11 +10,11 @@ import scipy.sparse as sparse
 class GasDriftTest(unittest.TestCase):
     def test_gas_drift(self):
         world_params = {
-            'world_size': 1000
+            'world_size': 100
         }
         world_params['carbon_dioxide_map'] = np.full(shape=(world_params['world_size'], world_params['world_size']),
                                                      fill_value=0)
-        for i in range(3000):
+        for i in range(1000):
             world_params['carbon_dioxide_map'][int(world_params['world_size'] * .25)][
                 int(world_params['world_size'] * .25)] += 1
             world_params['carbon_dioxide_map'][int(world_params['world_size'] * .25)][
@@ -30,7 +30,7 @@ class GasDriftTest(unittest.TestCase):
 
     def test_gas_drift_scipy_sparse_matrix(self):
         world_params = {
-            'world_size': 1000
+            'world_size': 100
         }
 
         carbon_additions_each_frame = np.full(shape=(world_params['world_size'], world_params['world_size']),
@@ -44,10 +44,10 @@ class GasDriftTest(unittest.TestCase):
                                                                dtype=int)
         carbon_additions_each_frame_sparse_matrix: csc_matrix = sparse.csc_matrix(carbon_additions_each_frame)
 
-        for i in range(3000):
+        for i in range(50):
             world_params['carbon_dioxide_map'] = world_params['carbon_dioxide_map'] + carbon_additions_each_frame_sparse_matrix
 
-            gd.move_gases_scipy_sparse_matrix(world_params['carbon_dioxide_map'], world_params['world_size'])
+            world_params['carbon_dioxide_map'] = gd.move_gases_scipy_sparse_matrix(world_params['carbon_dioxide_map'], world_params['world_size'])
 
         ar.render_array(world_params['carbon_dioxide_map'].toarray(), 'Carbon Dioxide')
 
