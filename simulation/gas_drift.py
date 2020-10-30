27fc38b4f13c5985a9584903bac242bdcb571ecd
Richard Williams <rwill128.create@gmail.com>
2020-10-30 14:20:41 -0400

    Adding performance comparison Jupyter. Things need to be cleaned up and organized very soon

--- simulation/gas_drift.py
+++ simulation/gas_drift.py
@@ -29,6 +29,27 @@ def move_gases(gas_map: np.array, world_size: int):
             gas_map[x][y] -= 1
             gas_map[i][j] += 1
 
+@nb.jit(nopython=True, fastmath=False)
+def move_gases_no_fastmath(gas_map: np.array, world_size: int):
+    gas_filled_squares = np.nonzero(gas_map)
+    for parallel_index in range(len(gas_filled_squares[0])):
+        x = gas_filled_squares[0][parallel_index]
+        y = gas_filled_squares[1][parallel_index]
+        possible_places_to_go = []
+        gas_value = gas_map[x][y]
+        if x > 0 and gas_map[x - 1][y] < gas_value:
+            possible_places_to_go.append((x - 1, y))
+        if x < world_size - 1 and gas_map[x + 1][y] < gas_value:
+            possible_places_to_go.append((x + 1, y))
+        if y > 0 and gas_map[x][y - 1] < gas_value:
+            possible_places_to_go.append((x, y - 1))
+        if y < world_size - 1 and gas_map[x][y + 1] < gas_value:
+            possible_places_to_go.append((x, y + 1))
+
+        if len(possible_places_to_go) > 0:
+            i, j = possible_places_to_go[np.random.randint(0, len(possible_places_to_go))]
+            gas_map[x][y] -= 1
+            gas_map[i][j] += 1
 
 # @nb.jit(nopython=True, fastmath=True)
 def move_gases_scipy_sparse_matrix(gas_map: sparse.csc_matrix, world_size: int):
@@ -47,7 +68,7 @@ def move_gases_scipy_sparse_matrix(gas_map: sparse.csc_matrix, world_size: int):
     return gas_map + modifications_matrix
 
 
-@nb.jit(nopython=True, fastmath=True)
+@nb.jit(nopython=True, fastmath=False)
 def numba_move_gases(x_squares: np.array, y_squares: np.array, gas_values: np.array, row_test: np.array,
                      col_test: np.array, data_test: np.array, world_size: int):
     for parallel_index in range(len(x_squares)):
