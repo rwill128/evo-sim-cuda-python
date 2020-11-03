ef6945cc462fc7545069af90456e0aa2117be16c
Richard Williams <rwill128.create@gmail.com>
2020-11-03 16:05:00 -0500

    Playing with very large size sims

--- run_simulation/run_simulation_test.py
+++ run_simulation/run_simulation_test.py
@@ -8,14 +8,14 @@ import plants.plant_rendering as pr
 
 class RunSimulationTest(unittest.TestCase):
     def test_run_simulation(self):
-        world_params = {'world_size': 200,
+        world_params = {'world_size': 1000,
                         'global_creature_id_counter': int(1)}
         world_params['world_array'] = np.zeros(shape=(world_params['world_size'], world_params['world_size']),
                                                dtype=int)
         world_params['carbon_dioxide_map'] = np.full(shape=(world_params['world_size'], world_params['world_size']),
                                                      fill_value=0)
         pc.spawn_new_plants(world_params=world_params,
-                            num_plants=100)
+                            num_plants=2000)
         pr.place_plants(world_params)
 
         rs.run_sim_for_x_steps(world_params, 10000)
