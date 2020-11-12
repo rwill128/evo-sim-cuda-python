810f9a2cbb64472fce17d0a720859cdddca66260
rwilliams <rwilliams@casenetllc.com>
2020-11-12 12:30:03 -0500

    Running a shorter but still longer sim.

--- run_simulation_test.py
+++ run_simulation_test.py
@@ -32,7 +32,7 @@ class RunSimulationTest(unittest.TestCase):
                             num_plants=500)
         pr.place_plants(world_params)
 
-        rs.run_sim_for_x_steps(world_params, 1000000)
+        rs.run_sim_for_x_steps(world_params, 200000)
 
 
 if __name__ == '__main__':
